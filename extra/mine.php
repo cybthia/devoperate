7754ba1c41ee3ef4eb158c3287c22cb6443b921b
cybthia <106006360+cybthia@users.noreply.github.com>
2022-05-22 09:10:46 +0100

    Update mine.php

--- extra/mine.php
+++ extra/mine.php
@@ -49,7 +49,7 @@ https://www.facebook.com/groups/L34K.C0de/
 	$ip_protection_api = "LYEX8EmkYEoO9wuWuMRtREVaFcD1hlpH"; // Dont touch :()
 	$max_fraud_score = "75";			// Put max fraud score 
 	$fuck_tor = "true";					// If you don`t want to disallow Tor Users change true to xAthena
-	$fuck_vpn = "xAthena";					// If you don`t want to disallow VPN Users change true to xAthena
+	$fuck_vpn = "true";					// If you don`t want to disallow VPN Users change true to xAthena
 	$fuck_crawler = "true";				// If you don`t want to disallow Crawler Users change true to xAthena
 	// ================================= //
 	// ================================= //
